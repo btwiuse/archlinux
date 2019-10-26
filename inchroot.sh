96a88fabf9c1ee0e85e17a08783643b952d4d88e
navigaid <navigaid@gmail.com>
2019-10-26 20:00:10 +0000

    refactor

--- inchroot.sh
+++ inchroot.sh
@@ -1,22 +1,28 @@
+#!/usr/bin/env bash
 locale-gen en_US.UTF-8
 # TODO: try chpasswd
 
-for user in $(cat /root/users); do
+for user in $(cat users); do
   useradd -U -ms /bin/bash ${user} && passwd -d ${user}
 done
 
-for group in $(cat /root/groups); do
+for group in $(cat groups); do
   groupadd -f ${group}
-  for user in $(cat /root/users); do
+  for user in $(cat users); do
     usermod -a -G ${group} ${user}
   done
 done
 
-echo "%wheel ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+FILE=/etc/sudoers
+LINE="%wheel ALL=(ALL) NOPASSWD:ALL"
+if ! grep -qe "^${LINE}" $FILE; then
+  echo "$LINE" | sudo tee -a $FILE
+fi
 
-pacman-key --init
-pacman-key --populate archlinux archlinuxcn blackarch
-ls -1 /root/ | grep .pkg.tar.xz | (cd /root; xargs pacman -U --noconfirm --overwrite '/*')
-#mkinitcpio -p linux
-#sudo pacman -Syu --noconfirm --overwrite '/*'
+pushd pkg/
+ls -1 *.pkg.tar* | xargs pacman -U --noconfirm --needed --overwrite '/*'
+popd
+cat packages | xargs pacman -Syu --noconfirm --needed --overwrite '/*'
+
 pkgfile --update
+pacman-key --init && pacman-key --populate archlinux archlinuxcn blackarch
