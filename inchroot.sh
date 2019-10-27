4ef0995eec764a745637aea116b31e3c8bb119b7
navigaid <navigaid@gmail.com>
2019-10-27 19:02:15 +0000

    inchroot: ensure pwd=/root/arch

--- inchroot.sh
+++ inchroot.sh
@@ -1,28 +1,47 @@
 #!/usr/bin/env bash
-locale-gen en_US.UTF-8
-# TODO: try chpasswd
+# chroot defaults to /
+cd /root/arch
 
-for user in $(cat users); do
-  useradd -U -ms /bin/bash ${user} && passwd -d ${user}
-done
+gen-locale(){
+  locale-gen en_US.UTF-8
+}
 
-for group in $(cat groups); do
-  groupadd -f ${group}
+ug(){
+  # TODO: try chpasswd
+
   for user in $(cat users); do
-    usermod -a -G ${group} ${user}
+    useradd -U -ms /bin/bash ${user} && passwd -d ${user}
   done
-done
 
-FILE=/etc/sudoers
-LINE="%wheel ALL=(ALL) NOPASSWD:ALL"
-if ! grep -qe "^${LINE}" $FILE; then
-  echo "$LINE" | sudo tee -a $FILE
-fi
+  for group in $(cat groups); do
+    groupadd -f ${group}
+    for user in $(cat users); do
+      usermod -a -G ${group} ${user}
+    done
+  done
 
-pushd pkg/
-ls -1 *.pkg.tar* | xargs pacman -U --noconfirm --needed --overwrite '/*'
-popd
-cat packages | xargs pacman -Syu --noconfirm --needed --overwrite '/*'
+  echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/wheel
+}
 
-pkgfile --update
-pacman-key --init && pacman-key --populate archlinux archlinuxcn blackarch
+setup-keyring(){
+  pacman-key --init && pacman-key --populate archlinux archlinuxcn blackarch
+}
+
+install-binpkg(){
+  ls -1 pkg/*.pkg.tar* | xargs pacman -U --noconfirm --needed --overwrite '/*'
+}
+
+update-packages(){
+./packages lite | xargs pacman -Syu --noconfirm --needed --overwrite '/*'
+# pkgfile --update
+}
+
+main(){
+  gen-locale
+  ug
+# setup-keyring
+# install-binpkg
+# update-packages
+}
+
+main "$@"
